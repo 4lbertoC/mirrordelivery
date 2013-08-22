69451ad9463b2b1196e6653d004b0bf4b475d327
4lbertoC <4lbertoc@gmail.com>
2013-08-22 22:23:36 +0200

    added package.zip and information on its size

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+PACKAGE_FILE_NAME=build/package.zip
+MAXSIZE=13312
+
 rm -rf build
 mkdir build
 java -jar compiler.jar --js scripts/jsfx/audio.js --js scripts/jsfx/jsfx.js --js scripts/jsfx/jsfxlib.js --js scripts/game.js --compilation_level ADVANCED_OPTIMIZATIONS > build/game.min.js
 cp index.html build
 cp -R img build
+zip -rq9 $PACKAGE_FILE_NAME build
+FILESIZE=$(stat -f "%z" "$PACKAGE_FILE_NAME")
+echo "Size of $PACKAGE_FILE_NAME is $FILESIZE bytes ($((100 * $FILESIZE/$MAXSIZE))% of $MAXSIZE bytes). Still $(($MAXSIZE - $FILESIZE)) available."
